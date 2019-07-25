8747acd1956858c3e025bdf96bb0936bb2b68155
qwerty11011 <53244490+qwerty11011@users.noreply.github.com>
2019-07-25 09:15:11 +0900

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 /*
-	Projection by TEMPLATED
+	Visualize by TEMPLATED
 	templated.co @templatedco
 	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
 */
 
-(function($) {
+$(function() {
+
+	// Vars.
+		var	$window = $(window),
+			$body = $('body'),
+			$wrapper = $('#wrapper');
 
 	// Breakpoints.
 		skel.breakpoints({
@@ -15,52 +20,30 @@
 			xsmall:	'(max-width: 480px)'
 		});
 
-	$(function() {
+	// Disable animations/transitions until everything's loaded.
+		$body.addClass('is-loading');
 
-		var	$window = $(window),
-			$body = $('body');
+		$window.on('load', function() {
+			$body.removeClass('is-loading');
+		});
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+	// Poptrox.
+		$window.on('load', function() {
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
+			$('.thumbnails').poptrox({
+				onPopupClose: function() { $body.removeClass('is-covered'); },
+				onPopupOpen: function() { $body.addClass('is-covered'); },
+				baseZIndex: 10001,
+				useBodyOverflow: false,
+				usePopupEasyClose: true,
+				overlayColor: '#000000',
+				overlayOpacity: 0.75,
+				popupLoaderText: '',
+				fadeSpeed: 500,
+				usePopupDefaultStyling: false,
+				windowMargin: (skel.breakpoint('small').active ? 5 : 50)
 			});
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+		});
 
-	// Off-Canvas Navigation.
-
-		// Navigation Panel.
-			$(
-				'<div id="navPanel">' +
-					$('#nav').html() +
-					'<a href="#navPanel" class="close"></a>' +
-				'</div>'
-			)
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left'
-				});
-
-		// Fix: Remove transitions on WP<10 (poor/buggy performance).
-			if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-				$('#navPanel')
-					.css('transition', 'none');
-
-	});
-
-})(jQuery);
+});
